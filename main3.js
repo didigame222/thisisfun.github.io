e19d126051eea282e3ee04150fa45a5c14fae796
didigame222 <arthur.nascimento33210@proton.me>
2024-05-23 19:00:37 -0500

    Add files via upload

---
+++ main3.js
@@ -0,0 +1,7 @@
+function openLinks() {
+    var i = 0;
+
+    while ( i < 500) {
+        window.open('http://www.youtube.com/watch?v=dQw4w9WgXcQ'); //doesn't work, should print 150, 151...
+    }
+}
